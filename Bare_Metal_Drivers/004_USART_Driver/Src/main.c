bd2c7db98510a914a8e34e43e64f7a527a357072
Soyabbir Abu Hanif <hanifseceee535@gmail.com>
2025-04-07 15:45:10 +0300

    usart testing

--- Bare_Metal_Drivers/004_USART_Driver/Src/main.c
+++ Bare_Metal_Drivers/004_USART_Driver/Src/main.c
@@ -3,21 +3,21 @@
 
  
 
-// void USART3_IRQHandler  (void){
-// 	char message = 'c';
-// 	message = USART_receive (2);
-// 	USART_transmit (2, message);
-// }
+void USART1_IRQHandler  (void){
+	char message = 'c';
+	message = USART_receive (1);
+	USART_transmit (1, message);
+}
 
 
 int main ()
 {
 	
-	//init_usart_receive_interrupt (2, 115200);
-	init_USART(2, 9600);
+	init_usart_receive_interrupt (1, 115200);
+	//init_USART(1, 9600);
 	
 	while (1){
-		USART_transmit(2, 'c');
+		//USART_transmit(1, 'R');
 		
 		}
 	
